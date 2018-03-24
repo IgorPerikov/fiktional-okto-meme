908d277b0492c3bfc50cc7323992536ed28ce900
Igor Perikov <perikov.igor@gmail.com>
2018-03-24 16:19:28 +0300

    add travis build

--- build.sh
+++ build.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-docker build -f Dockerfile_app -t oktomeme/oktomeme:latest .
+export NAME=igorperikov
+export IMAGE=oktomeme
+docker build -t $NAME/$IMAGE:latest .
+if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ] ; then
+    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+    docker tag $NAME/$IMAGE:latest $NAME/$IMAGE:$TRAVIS_BUILD_NUMBER
+    docker push $NAME/$IMAGE:latest
+    docker push $NAME/$IMAGE:$TRAVIS_BUILD_NUMBER
+fi
