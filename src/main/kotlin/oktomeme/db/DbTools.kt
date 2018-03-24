f6156667dfbd58b62297c609ab292e09f1f37a23
Igor Perikov <perikov.igor@gmail.com>
2018-03-24 13:32:17 +0300

    introduce jdbc configuration via env

--- src/main/kotlin/oktomeme/db/DbTools.kt
+++ src/main/kotlin/oktomeme/db/DbTools.kt
@@ -5,12 +5,13 @@ import com.zaxxer.hikari.HikariDataSource
 import org.flywaydb.core.Flyway
 import org.jetbrains.exposed.sql.Database
 import org.slf4j.LoggerFactory
-import java.util.*
 
 object DbTools {
     lateinit var dataSource: HikariDataSource
 
-    val log = LoggerFactory.getLogger(this::class.java)
+    private const val DEFAULT_JDBC_URL: String =
+        "jdbc:postgresql://localhost:5432/postgres?user=root&password=root&ssl=false"
+    private val log = LoggerFactory.getLogger(this::class.java)
 
     fun init() {
         log.info("Start db init script")
@@ -20,9 +21,10 @@ object DbTools {
     }
 
     private fun setupConnectionPool() {
-        val hikariPropertiesStream = DbTools::class.java.classLoader.getResourceAsStream("hikari.properties")
-        val hikariProperties = Properties().apply { this.load(hikariPropertiesStream) }
-        val config = HikariConfig(hikariProperties)
+        val config = HikariConfig()
+        config.jdbcUrl = System.getenv("JDBC_URL") ?: DEFAULT_JDBC_URL
+        config.maximumPoolSize = 10
+
         dataSource = HikariDataSource(config)
         Database.connect(dataSource)
     }
