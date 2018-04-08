ded55da708e3baa55ac0705fa05f2d2cd164c57d
Igor Perikov <perikov.igor@gmail.com>
2018-04-08 23:11:22 +0300

    add requests and providers routes with pagination

--- src/main/kotlin/oktomeme/Server.kt
+++ src/main/kotlin/oktomeme/Server.kt
@@ -9,6 +9,7 @@ import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
 import io.ktor.features.StatusPages
+import io.ktor.http.HttpMethod
 import io.ktor.http.HttpStatusCode
 import io.ktor.jackson.jackson
 import io.ktor.locations.Location
@@ -16,6 +17,9 @@ import io.ktor.locations.Locations
 import io.ktor.locations.get
 import io.ktor.response.respond
 import io.ktor.routing.Routing
+import io.ktor.routing.method
+import io.ktor.routing.optionalParam
+import io.ktor.routing.route
 import oktomeme.db.DbTools
 import oktomeme.db.ProvidersService
 import oktomeme.db.RequestsService
@@ -50,6 +54,38 @@ fun Application.main() {
         }
     }
     install(Routing) {
+        route("/providers") {
+            method(HttpMethod.Get) {
+                optionalParam("perPage") {
+                    optionalParam("page") {
+                        handle {
+                            call.respond(
+                                ProvidersService.getProviders(
+                                    call.request.queryParameters["perPage"]?.toInt() ?: 10,
+                                    call.request.queryParameters["page"]?.toInt() ?: 0
+                                )
+                            )
+                        }
+                    }
+                }
+            }
+        }
+        route("/requests") {
+            method(HttpMethod.Get) {
+                optionalParam("perPage") {
+                    optionalParam("page") {
+                        handle {
+                            call.respond(
+                                RequestsService.getRequests(
+                                    call.request.queryParameters["perPage"]?.toInt() ?: 10,
+                                    call.request.queryParameters["page"]?.toInt() ?: 0
+                                )
+                            )
+                        }
+                    }
+                }
+            }
+        }
         get<ProviderById> { providerRequest ->
             val provider = ProvidersService.getProviderById(providerRequest.id)
             call.respond(provider)
